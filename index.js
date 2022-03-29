8963d0280a5e9533f338e4fef79b2ec87cda5dfc
luciodicosco <dicoscolucho@gmail.com>
2022-03-29 20:10:31 -0300

    agregué ejemplos y funciones

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ if (variable2 < variable1){
 
 } */
 
+/* function saludar(){
+    alert("Bienvenido");
+}
+
+saludar(nombreUsuario); */
+
 // Si el usuario escribe, se ejecuta else //
 /* EJEMPLO 2 */
 let nombreUsuario = prompt("Ingresar nombre de usuario");
@@ -16,8 +22,10 @@ if (nombreUsuario == "") {
     alert("No se ingresó el nombre de usuario");
 }
 else{
-    alert("Usuario: " + nombreUsuario);
+    alert("Bienvenido " + nombreUsuario);
 } 
+
+
 
 /*EJEMPLO 3
 let variable1 = 5;
@@ -59,3 +67,35 @@ while (i < 10){
     console.log(i);
     i++;
 } */
+
+/* EJEMPLO SUMA
+function sumar(numA, numB){
+    let resultado = numA + numB;
+    return resultado;
+} */
+
+/* EJEMPLO RESTA
+function restar(numA, numB){
+    let resultado = numA - numB;
+    return resultado;
+} */
+
+/* EJEMPLO CONST 
+const suma = function (numA, numB) {return numA + numB}
+console.log(suma(10+5)); */
+
+/* EJEMPLO CONST 2
+const resta = function (numA, numB) {return numA - numB}
+console.log(resta(10+5)); */
+
+/* EJEMPLO CONST IVA 
+
+const IVA = (precio) => {return precio*0.21} */
+
+const IVA = precio => precio*0.21
+function precioTotal (precio,IVA){
+    return precio+IVA;
+}
+let precioProducto = 1250;
+precioTotal(precioProducto,IVA(precioProducto))
+console.log(IVA(1500));
